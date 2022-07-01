573de7b173e98588e1dc62c55fb45f4113e41bde
Vyomesh-4286 <103420541+Vyomesh-4286@users.noreply.github.com>
2022-07-01 11:58:34 +0530

    frist commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Gotham: 'Gotham',
+        Minion: 'Minion Pro',
+      },
+      spacing: {
+        15: '3.75rem',
+        per: '15% auto',
+      },
+    },
+  },
+  plugins: [],
+}
